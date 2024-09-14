c38ce2e83de883782d462acc7d0ae8fc430181c3
MARCUS VINICIUS PIMENTEL DE FREITAS <021.900244@senaicimatec.edu.br>
2024-09-14 10:10:05 -0300

    aaa

---
+++ FeaASP/Models/ApplicationDbContext.cs.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace FEAasp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<contato> contatos { get; set; }
+        
+        internal bool TestConnection()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FeaASP/Models/Parceiro.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FEAasp.Models
+{
+    public class Parceiro
+    {
+        public int idParceiro { get; set; }
+        public string nomeEmpresa { get; set; }
+        public int CNPJ { get; set; }
+        public int telefoneEmpresa { get; set; }
+
+        public string EmailEmpresa { get; set; }
+
+        public string Website { get; set; }
+    }
+}
